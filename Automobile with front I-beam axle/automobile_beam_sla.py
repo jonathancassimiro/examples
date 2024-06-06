4b6fdea03642acf9f43ae0fef476cb3270a27216
jonathancassimiro <112102452+jonathancassimiro@users.noreply.github.com>
2024-06-05 23:54:29 -0300

    Add files via upload

---
+++ Automobile with front I-beam axle/automobile_beam_sla.py
@@ -0,0 +1,60 @@
+from codes import SLA, VA
+import numpy as np
+import time
+
+execucao = time.time()
+Geval = 0
+
+# AUTOMOBILE WITH FRONT I-BEAM AXLE
+
+# OBJECTIVE FUNCTION
+def f(d, x):
+    return x[3] * x[0] + 2. * (x[1] - x[0]) * x[2]
+
+# JACOBIAN OF THE OBJECTIVE FUNCTION
+def df(d, x):
+    df_x1 = x[3] - 2. * x[2]
+    df_x2 = 2. * x[2]
+    df_x3 = 2. * x[1] - 2. * x[0]
+    df_x4 = x[0]
+    return np.array([df_x1, df_x2, df_x3, df_x4])
+
+# LIMIT STATES FUNCTIONS
+def g(d, x, p):
+    global Geval
+    Geval += 1
+    Wx = x[0] * (x[3] - 2. * x[2]) ** 3 / (6. * x[3]) + x[1] * (x[3] ** 3 - (x[3] - 2. * x[2]) ** 3) / (6. * x[3])
+    Wp = 0.8 * x[1] * x[2] ** 2 + 0.4 * x[0] **3 * (x[3] - 2. * x[2]) / x[2]
+    gamas = 460.
+    gamam = p[0] / Wx
+    tau = p[1] / Wp
+    return [gamas - np.sqrt((gamam ** 2) + 3. * (tau ** 2))]
+
+# TARGET RELIABILITY INDEX
+betaT = [3.]
+
+# BOUNDS
+bndsX = [[10., 20.], [60., 100.], [10., 20.], [70., 120.]]
+
+# INITIAL POINTS
+x0 = [12., 75., 12., 85.]
+
+# RANDOM DESIGN VARIABLES, x = {x1, x2, x3, x4}
+x1 = VA('Normal', mean = x0[0], sdev = 0.060)
+x2 = VA('Normal', mean = x0[1], sdev = 0.325)
+x3 = VA('Normal', mean = x0[2], sdev = 0.070)
+x4 = VA('Normal', mean = x0[3], sdev = 0.425)
+x = [x1, x2, x3, x4]
+
+# RANDOM DESIGN PARAMETERS, p = {p1, p2}
+p1 = VA('Normal', mean = 3.5e6, sdev = 1.75e5)
+p2 = VA('Normal', mean = 3.1e6, sdev = 1.55e5)
+p = [p1, p2]
+
+# SLA FUNCTION
+res = SLA(f, df, g, betaT, x = x, p = p, bndsX = bndsX, disp = True)
+
+# RESULTS
+print(res)
+print('Processing Time:', round((time.time() - execucao),3), 's')
+print('Limit State Functions Evaluations:', Geval)
---
+++ Hock and Schittkowski 113/hock_schittkowski _pma.py
@@ -0,0 +1,108 @@
+from codes import AnalysisOptions, StochasticModel, LimitState, Form, Normal
+from scipy.optimize import minimize
+import numpy as np
+import time
+
+execucao = time.time()
+Geval = 0
+
+# HOCK AND SCHITTKOWSKI 113
+
+def lsf(x, g):
+    PMA = True
+    options = AnalysisOptions()
+    options.is_PMA = PMA
+    options.beta_targ = betaT
+    stochastic_model = StochasticModel()
+    stochastic_model.addVariable(Normal('x1', x[0],  0.02**1))
+    stochastic_model.addVariable(Normal('x2', x[1],  0.02**1))
+    stochastic_model.addVariable(Normal('x3', x[2],  0.02**1))
+    stochastic_model.addVariable(Normal('x4', x[3],  0.02**1))
+    stochastic_model.addVariable(Normal('x5', x[4],  0.02**1))
+    stochastic_model.addVariable(Normal('x6', x[5],  0.02**1))
+    stochastic_model.addVariable(Normal('x7', x[6],  0.02**1))
+    stochastic_model.addVariable(Normal('x8', x[7],  0.02**1))
+    stochastic_model.addVariable(Normal('x9', x[8],  0.02**1))
+    stochastic_model.addVariable(Normal('x10', x[9],  0.02**1))
+    limit_state = LimitState(g)
+    Analysis = Form(analysis_options=options, stochastic_model=stochastic_model, limit_state=limit_state)
+    Analysis.run()
+    return Analysis.G
+
+# OBJECTIVE FUNCTION
+def f(x):
+    return x[0]**2 + x[1]**2 + x[0] * x[1] - 14. * x[0] - 16. * x[1] + (x[2] - 10.)**2 + 4. * (x[3] - 5.)**2 + (x[4] - 3.)**2 + 2. * (x[5] - 1.)**2 + 5. * x[6]**2 + 7. * (x[7] - 11.)**2 + 2. * (x[8] - 10.)**2 + (x[9] - 7.)**2 + 45.
+
+# JACOBIAN OF THE OBJECTIVE FUNCTION
+def df(x):
+    df_dx1 = 2. * x[0] + x[1] - 14.
+    df_dx2 = 2. * x[1] + x[0] - 16.
+    df_dx3 = 2. * x[2] - 20.
+    df_dx4 = 8. * x[3] - 40.
+    df_dx5 = 2. * x[4] - 6.
+    df_dx6 = 4. * x[5] - 4.
+    df_dx7 = 10. * x[6]
+    df_dx8 = 14. * x[7] - 154.
+    df_dx9 = 4. * x[8] - 40.
+    df_dx10 = 2. * x[9] - 14.
+    return np.array([[df_dx1, df_dx2, df_dx3, df_dx4, df_dx5, df_dx6, df_dx7, df_dx8, df_dx9, df_dx10]])
+
+# LIMIT STATES FUNCTIONS
+def g1(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10):
+    global Geval
+    Geval += 1
+    return 1. - (4. * x1 + 5. * x2 - 3. * x7 + 9. * x8) / 105.
+def g2(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10):
+    global Geval
+    Geval += 1
+    return -10. * x1 + 8. * x2 + 17. * x7 - 2. * x8
+def g3(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10):
+    global Geval
+    Geval += 1
+    return 1. + (8. * x1 - 2. * x2 - 5. * x9 + 2. * x10) / 12.
+def g4(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10):
+    global Geval
+    Geval += 1
+    return 1. - (3. * (x1 - 2.)**2 + 4. * (x2 - 3.)**2 + 2. * x3**2 - 7. * x4) / 120.
+def g5(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10):
+    global Geval
+    Geval += 1
+    return 1. - (5. * x1**2 + 8. * x2 + (x3 - 6)**2 - 2. * x4) / 40.
+def g6(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10):
+    global Geval
+    Geval += 1
+    return 1. - (0.5 * (x1 - 8.)**2 + 2. * (x2 - 4.)**2 + 3. * x5**2 - x6) / 30.
+def g7(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10):
+    global Geval
+    Geval += 1
+    return -x1**2 - 2. * (x2 - 2.)**2 + 2. * x1 * x2 - 14. * x5 + 6. * x6
+def g8(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10):
+    global Geval
+    Geval += 1
+    return 3. * x1 - 6. * x2 - 12. * (x9 - 8.)**2 + 7. * x10
+
+cons = ({'type': 'ineq', 'fun': lambda x: lsf(x, g1)},
+        {'type': 'ineq', 'fun': lambda x: lsf(x, g2)},
+        {'type': 'ineq', 'fun': lambda x: lsf(x, g3)},
+        {'type': 'ineq', 'fun': lambda x: lsf(x, g4)},
+        {'type': 'ineq', 'fun': lambda x: lsf(x, g5)},
+        {'type': 'ineq', 'fun': lambda x: lsf(x, g6)},
+        {'type': 'ineq', 'fun': lambda x: lsf(x, g7)},
+        {'type': 'ineq', 'fun': lambda x: lsf(x, g8)})
+
+# TARGET RELIABILITY INDEX
+betaT = [3.]
+
+# BOUNDS
+bnds = [[0, np.inf], [0, np.inf], [0, np.inf], [0, np.inf], [0, np.inf], [0, np.inf], [0, np.inf], [0, np.inf], [0, np.inf], [0, np.inf]]
+
+# INITIAL POINT
+x0 = [2.17, 2.36, 8.77, 5.10, 0.99, 1.43, 1.32, 9.83, 8.28, 8.38]
+
+# OPTIMIZATION
+res = minimize(fun=f, jac=df, x0=x0, method='slsqp', bounds=bnds, constraints=cons, options={'disp': True, 'maxiter':1000, 'ftol': 1e-4})
+
+# RESULTS
+print(res)
+print('Processing Time:', round((time.time() - execucao),3), 's')
+print('Limit State Functions Evaluations:', Geval)
